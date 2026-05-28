4d854c6c20806ad59deeea9aa64af2285e4e928e
JakeOh <jake.oh.lecture@gmail.com>
2026-05-28 15:26:35 +0900

    where clauses

--- lab_sql/sql03_where.sql
+++ lab_sql/sql03_where.sql
@@ -51,3 +51,41 @@ select ename, job, sal
 from emp
 where sal >= 2000 and sal <= 3000
 order by sal desc;
+
+-- between A and B 연산자: 컬럼이름 between 값1 and 값2
+select ename, job, sal
+from emp
+where sal between 2000 and 3000
+order by sal desc;
+
+-- 수당은 null이 아니고, 급여는 1500 미만인 직원들의 모든 정보를 출력.
+select * from emp
+where comm is not null and sal < 1500;
+
+-- 10번 또는 20번 부서에서 근무하는 직원들의 부서번호, 이름, 급여를 출력.
+-- 정렬: (1) 부서번호 오름차순, (2) 급여 내림차순.
+select deptno, ename, sal
+from emp
+where deptno = 10 or deptno = 20
+order by deptno, sal desc;
+
+select deptno, ename, sal
+from emp
+where deptno in (10, 20)
+order by deptno, sal desc;
+
+-- 업무가 'CLERK'인 직원들의 이름, 업무, 급여를 출력. 이름순으로 출력.
+select ename, job, sal
+from emp
+where job = 'CLERK';
+--> 문자열은 작은따옴표('')를 사용함!
+--> 컬럼에 저장된 문자열의 비교에서는 대/소문자를 구별!!
+
+-- 업무가 'CLERK' 또는 'MANAGER'인 직원들의 이름, 업무, 급여를 출력.
+-- 정렬: (1) 업무, (2) 급여
+
+-- 업무가 영업사원(SALESMAN), 관리자(MANAGER), 분석가(ANALYST)인 직원들의 모든 정보(컬럼).
+
+-- 20번 부서에서 근무하는 'CLERK'의 모든 레코드(모든 컬럼)를 출력.
+
+-- CLERK, ANALYST, MANAGER가 아닌 직원들의 사번, 이름, 업무, 급여를 사번순 출력.
