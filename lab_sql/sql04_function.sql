ea358fd3d2402ce3adc0eda5edadfe19b8915cf5
JakeOh <jake.oh.lecture@gmail.com>
2026-05-29 12:20:34 +0900

    substr, length, to_date

--- lab_sql/sql04_function.sql
+++ lab_sql/sql04_function.sql
@@ -60,3 +60,35 @@ select length('hello'), lengthb('hello') from dual;
 select length('안녕하세요'), lengthb('안녕하세요') from dual;
 --> 영문자(a, b, A, B, ...), 숫자(0, 1, 2, ...), 특수기호(!, @, #, ...)들은 오라클에 저장될 때 1바이트가 사용됨.
 --> 한글 1글자는 3바이트가 사용됨.
+
+-- 직원 이름이 6글자 이상인 직원들의 레코드.
+select * from emp where length(ename) >= 6;
+
+-- 직원 이름의 첫글자와 마지막 글자만 표기하고 중간은 '*'로만 출력.
+select 
+    ename, substr(ename, 1, 1), substr(ename, length(ename), 1)
+from emp;
+
+select
+    substr(ename, 1, 1) || '*' || substr(ename, length(ename), 1) as "이름"
+from emp;
+
+
+-- to_date(문자열, '날짜 포맷'): '날짜 포맷' 형식으로 작성된 문자열을 날짜(date) 타입으로 변환.
+select to_date('2026-05-29', 'YYYY-MM-DD') from dual;
+select to_date('05-29-2026', 'MM-DD-YYYY') from dual;
+select to_date('05-08-2026', 'DD-MM-YYYY') from dual;
+
+-- 연도 2자리 표기법(YY와 RR의 차이점)
+-- YY(현재 세기): 현재 세기의 끝 두 자리 연도.
+-- RR(반올림 세기): 반올림해서 현재 세기가 될 수 있는 연도의 끝 두 자리.
+--   현재 세기(21세기)
+--   반올림해서 21세기가 되는 연도들: 1950 ~ 2049
+
+select to_date('99-11-20', 'YY-MM-DD') from dual;  --> 2099-11-20
+select to_date('99-11-20', 'RR-MM-DD') from dual;  --> 1999-11-20
+select to_date('49-05-29', 'RR-MM-DD') from dual;  --> 2049-05-29
+
+-- 1981 ~ 1982 사이에 입사한 직원들의 레코드.
+select * from emp
+where hiredate between to_date('81/01/01', 'RR/MM/DD') and to_date('82/12/31', 'RR/MM/DD');
