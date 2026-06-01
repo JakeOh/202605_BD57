525587dfe1737a835e282c254b44b8b99835a8d7
JakeOh <jake.oh.lecture@gmail.com>
2026-06-01 11:22:33 +0900

    집계 함수. count, sum, avg, variance, stddev, max, min.

--- lab_sql/sql05_groupby.sql
+++ lab_sql/sql05_groupby.sql
@@ -16,3 +16,45 @@ select count(*) from emp;
 -- sum(column): column에서 null 아닌 값들의 합계를 반환.
 select sum(sal) from emp;
 select sum(comm) from emp;
+
+-- avg(column): 평균(average, mean). 평균(avg) = 합계(sum) / 개수(count).
+select avg(sal) from emp;  -- 급여 평균
+select round(avg(sal), 2) from emp;
+
+select avg(comm) from emp;  -- 수당 평균(= 2200 / 4)
+
+-- 급여의 개수, 합계, 평균, 분산, 표준편차(standard deviation), 최댓값, 최솟값 출력
+select
+    count(sal) as 개수,
+    sum(sal) as 합계,
+    round(avg(sal), 2) as 평균,
+    round(variance(sal), 2) as 분산,
+    round(stddev(sal), 2) as 표준편차,
+    max(sal) as 최댓값,
+    min(sal) as 최솟값
+from emp;
+
+
+-- 다중 행 함수는 단일 행 함수 (또는 컬럼)과 함께 사용할 수 없음!
+-- select comm, count(comm) from emp;  --> 에러 발생.
+-- select nvl(comm, 0), count(comm) from emp;  --> 에러 발생.
+
+
+/*
+ * 그룹 쿼리
+ * (예) 부서 별 직원 수, 부서 별 급여 평균, ...
+ * (문법)
+ * select ...
+ * from 테이블
+ * where 조건식(1)
+ * group by 컬럼(그룹을 묶기 위한 컬럼), ...
+ * having 조건식(2)
+ * order by 컬럼(정렬 기준이 되는 컬럼), ...;
+ *
+ * where 조건식(1): 그룹을 묶기 전에 조건을 만족하는 행들만 선택하기 위한 조건식.
+ * having 조건식(2): 그룹을 묶은 다음에 조건에 맞는 그룹들만 선택하기 위한 조건식.
+ *
+ * (주의)
+ * group by에서 사용한 그룹을 묶기 위한 컬럼들은 select할 수 있음.
+ * group by에서 사용되지 않은 컬럼들은 select할 수 없음!!!
+ */
