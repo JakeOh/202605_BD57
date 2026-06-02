47ece017ac2d428d8099d057b4defc0d10f805ec
JakeOh <jake.oh.lecture@gmail.com>
2026-06-02 11:23:16 +0900

    join 연습

--- lab_sql/sql07_join_ex.sql
+++ lab_sql/sql07_join_ex.sql
@@ -49,8 +49,51 @@ from emp e
     full join dept d on e.deptno = d.deptno
 order by d.loc;
 
+select e.ename, d.loc
+from emp e, dept d
+where e.deptno = d.deptno(+)
+union
+select e.ename, d.loc
+from emp e, dept d
+where e.deptno(+) = d.deptno
+order by loc;
+
 -- Ex2. 직원 이름, 매니저의 이름, 직원 급여, 직원 급여 등급 검색. inner join.
 -- 정렬 순서: (1) 매니저, (2) 급여 등급
+select
+    e1.ename as "직원 이름",
+    e2.ename as "매니저 이름",
+    e1.sal as "직원 급여",
+    s.grade as "급여 등급"
+from emp e1
+    join emp e2 on e1.mgr = e2.empno
+    join salgrade s on e1.sal between s.losal and s.hisal
+order by "매니저 이름", "급여 등급";
+
+select
+    e1.*, e2.*, s.*
+from emp e1
+    join emp e2 on e1.mgr = e2.empno
+    join salgrade s on e1.sal between s.losal and s.hisal;
+
+select
+    e1.ename as "직원 이름",
+    e2.ename as "매니저 이름",
+    e1.sal as "직원 급여",
+    s.grade as "급여 등급"
+from emp e1, emp e2, salgrade s
+where e1.mgr = e2.empno and e1.sal between s.losal and s.hisal
+order by "매니저 이름", "급여 등급";
+
+select
+    e1.ename as "직원 이름",
+    e2.ename as "매니저 이름",
+    e1.sal as "직원 급여",
+    s.grade as "급여 등급"
+from emp e1, emp e2, salgrade s
+where e1.mgr = e2.empno(+) 
+    and e1.sal between s.losal(+) and s.hisal(+)
+order by "매니저 이름", "급여 등급";
 
 -- Ex3. 직원 이름, 부서 이름, 급여, 급여 등급 검색. inner join.
 -- 정렬 순서: (1) 부서 이름, (2) 급여 등급
