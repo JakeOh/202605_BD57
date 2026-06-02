a69646755f76894e1e5fa09bcef569a538b98c33
JakeOh <jake.oh.lecture@gmail.com>
2026-06-02 10:30:13 +0900

    join 연습

--- lab_sql/sql07_join_ex.sql
+++ lab_sql/sql07_join_ex.sql
@@ -2,8 +2,52 @@
  * JOIN 연습 문제
  */
 
--- Ex1. 직원 이름, 근무 도시를 검색. 도시 이름으로 정렬. 
+-- Ex1. 직원 이름, 근무 도시를 검색. 도시 이름으로 정렬.
 -- inner join과 outer join의 결과를 비교하세요.
+-- inner join
+select 
+    e.ename, d.loc
+from emp e 
+    join dept d on e.deptno = d.deptno
+order by d.loc;
+--> 14개 행
+
+select e.ename, d.loc
+from emp e, dept d
+where e.deptno = d.deptno
+order by d.loc;
+
+-- left outer join
+select 
+    e.ename, d.loc
+from emp e 
+    left join dept d on e.deptno = d.deptno
+order by d.loc;
+--> 15개 행(오쌤/null)
+
+select e.ename, d.loc
+from emp e, dept d
+where e.deptno = d.deptno(+)
+order by d.loc;
+
+-- right outer join
+select
+    e.ename, d.loc
+from emp e
+    right join dept d on e.deptno = d.deptno
+order by d.loc;
+--> 15개 행(null/boston)
+
+select e.ename, d.loc
+from emp e, dept d
+where e.deptno(+) = d.deptno
+order by d.loc;
+
+select
+    e.ename, d.loc
+from emp e
+    full join dept d on e.deptno = d.deptno
+order by d.loc;
 
 -- Ex2. 직원 이름, 매니저의 이름, 직원 급여, 직원 급여 등급 검색. inner join.
 -- 정렬 순서: (1) 매니저, (2) 급여 등급
