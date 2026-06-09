d1a3c2ee96878e342826d7887733d1c802999394
JakeOh <jake.oh.lecture@gmail.com>
2026-06-09 16:25:57 +0900

    create table. 기본값, 제약조건 설정 연습문제.

--- lab_sql/sql13_constraint.sql
+++ lab_sql/sql13_constraint.sql
@@ -168,8 +168,8 @@ select * from ex_contents;
  * 연습문제 1.
  * 테이블 이름: customers(고객)
  * 컬럼:
- * (1) cust_id: 고객 아이디. 8 ~ 20 byte의 문자열. primary key.
- * (2) cust_pw: 고객 비밀번호. 8 ~ 20 byte의 문자열. not null.
+ * (1) cust_id: 고객 아이디. 8 ~ 20 char의 문자열. primary key.
+ * (2) cust_pw: 고객 비밀번호. 8 ~ 20 char의 문자열. not null.
  * (3) cust_email: 고객 이메일. 100 byte 가변 길이 문자열.
  * (4) cust_gender: 고객 성별. 1자리 정수. 기본값 0. (0, 1, 2) 중 1개 값만 가능.
  * (5) cust_age: 고객 나이. 3자리 정수. 기본값 0. 0 이상 200 이하의 정수만 가능.
@@ -183,7 +183,7 @@ select * from ex_contents;
  * (1) order_id: 주문번호. 10자리 정수. primary key.
  * (2) order_date: 주문 날짜. 기본값은 현재 시간.
  * (3) order_method: 주문 방법. 최대 8 byte 문자열. ('online', 'offline') 중 1개 값만 가능.
- * (4) cust_id: 주문 고객 아이디. 최대 20 byte 문자열. not null. customers(cust_id)를 참조.
+ * (4) cust_id: 주문 고객 아이디. 최대 20 char 문자열. not null. customers(cust_id)를 참조.
  */
  
  
