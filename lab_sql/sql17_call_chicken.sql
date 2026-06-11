0804e85160f4e941050c197665d1f1a1d7349ea2
JakeOh <jake.oh.lecture@gmail.com>
2026-06-11 17:35:51 +0900

    치킨 주문 통화 건수 분석

--- lab_sql/sql17_call_chicken.sql
+++ lab_sql/sql17_call_chicken.sql
@@ -32,3 +32,86 @@ create table call_chicken (
 -- [접속] 패널 > call_chicken 테이블 오른쪽 클릭 > 데이터 임포트
 
 -- 4. 분석
+select * from call_chicken;
+
+select count(*) from call_chicken;
+--> 행(row) 개수
+
+-- 날짜들
+select distinct call_date from call_chicken order by call_date;
+select count(distinct call_date) from call_chicken;
+
+-- 요일들
+select distinct call_day from call_chicken;
+select count(distinct call_day) from call_chicken;
+
+-- 구 이름들
+select distinct gu from call_chicken order by gu;
+select count(distinct gu) from call_chicken;
+
+-- 연령대들
+select distinct ages from call_chicken order by ages;
+select count(distinct ages) from call_chicken;
+
+-- 성별들
+select distinct gender from call_chicken;
+select count(distinct gender) from call_chicken;
+
+-- (1) 통화 건수의 최댓값, 최솟값
+select max(calls), min(calls) from call_chicken;
+
+
+-- (2) 통화 건수가 최댓값이거나 최솟값인 레코드(행) 출력
+select * from call_chicken
+where calls = (select max(calls) from call_chicken)
+    or
+    calls = (select min(calls) from call_chicken);
+
+-- (3) 요일별 통화 건수 평균
+select 
+    call_day,
+    round(avg(calls), 2) as "CALLS"
+from call_chicken
+group by call_day
+order by CALLS desc;
+
+-- (4) 연령대별 통화 건수 평균
+select 
+    ages,
+    round(avg(calls), 2) as "CALLS"
+from call_chicken
+group by ages
+order by CALLS desc;
+
+-- (5) 요일별, 연령대별 통화 건수 평균
+select
+    call_day, ages,
+    round(avg(calls), 2) as "CALLS"
+from call_chicken
+group by call_day, ages
+order by CALLS desc;
+
+select
+    call_day, ages,
+    round(avg(calls), 2) as "CALLS"
+from call_chicken
+group by call_day, ages
+order by CALLS desc
+offset 0 rows
+fetch next 10 rows only;
+
+-- (6) 구별, 성별 통화 건수 평균
+select
+    gu, gender,
+    round(avg(calls), 2) as "CALLS"
+from call_chicken
+group by gu, gender
+order by CALLS desc;
+
+-- (7) 요일별, 구별, 연령대별 통화 건수 평균
+select
+    call_day, gu, ages,
+    round(avg(calls), 2) as "CALLS"
+from call_chicken
+group by call_day, gu, ages
+order by CALLS desc;
